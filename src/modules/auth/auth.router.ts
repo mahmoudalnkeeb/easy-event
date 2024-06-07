f528b804d0d77564a47e115cf995f855b34f8577
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2024-06-07 11:06:20 +0300

    refactor:change control panel login/signup path

--- src/modules/auth/auth.router.ts
+++ src/modules/auth/auth.router.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
-import { adminLoginController, adminSignupController, userLoginController, userSignupController } from './auth.controller';
+import {
+  adminLoginController,
+  adminSignupController,
+  userLoginController,
+  userSignupController,
+} from './auth.controller';
 
 const authRouter = Router();
 
 authRouter.post('/login', userLoginController);
 authRouter.post('/signup', userSignupController);
-authRouter.post('/cp/login' , adminLoginController)
-authRouter.post('/cp/signup' , adminSignupController)
+authRouter.post('/cp/login', adminLoginController);
+authRouter.post('/cp/signup', adminSignupController);
 
 export default authRouter;
