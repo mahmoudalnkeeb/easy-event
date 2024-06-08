29de90b81004438609a71fe5a5b8f573353311aa
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2024-06-08 12:32:23 +0300

    feat:cp signup require secret key

--- src/modules/auth/auth.router.ts
+++ src/modules/auth/auth.router.ts
@@ -5,12 +5,20 @@ import {
   userLoginController,
   userSignupController,
 } from './auth.controller';
+<<<<<<< HEAD
+=======
+import { requireSecretKey } from '../../middlewares/access.middleware';
+>>>>>>> 5bd7998 (feat:cp signup require secret key)
 
 const authRouter = Router();
 
 authRouter.post('/login', userLoginController);
 authRouter.post('/signup', userSignupController);
 authRouter.post('/cp/login', adminLoginController);
+<<<<<<< HEAD
 authRouter.post('/cp/signup', adminSignupController);
+=======
+authRouter.post('/cp/signup',requireSecretKey, adminSignupController);
+>>>>>>> 5bd7998 (feat:cp signup require secret key)
 
 export default authRouter;
